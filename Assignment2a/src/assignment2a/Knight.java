49d636bb9f12242c4ebb122b494989a6a630d33c
Howie Chan <hw.chan@hotmail.com>
2015-11-07 18:51:09 -0800

    Added knight movement

--- Assignment2a/src/assignment2a/Knight.java
+++ Assignment2a/src/assignment2a/Knight.java
@@ -10,4 +10,28 @@ public class Knight extends GamePiece {
     public String getType(){
         return "Knight";
     }
+    
+    @Override
+    public boolean isValidMove(Tile from, Tile to) {
+        int fromX = from.getXCoord();
+        int fromY = from.getYCoord();
+        int toX = to.getXCoord();
+        int toY = to.getYCoord();
+        
+        if(to.getPiece() == null || to.getPiece().isPlayer1() != isPlayer1()) {
+            // jumping L shape movement
+            if (fromX+2 == toX && fromY+1 == toY ||
+                fromX-2 == toX && fromY+1 == toY ||
+                fromX+1 == toX && fromY+2 == toY ||
+                fromX-1 == toX && fromY+2 == toY ||
+                    
+                fromX+2 == toX && fromY-1 == toY ||
+                fromX-2 == toX && fromY-1 == toY ||
+                fromX+1 == toX && fromY-2 == toY ||
+                fromX-1 == toX && fromY-2 == toY) {
+                return true;
+            }
+        } 
+        return false;
+    }
 }
