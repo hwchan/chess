87ea108fd18bf5ef80f5f66b535ac96b173d704f
Howie Chan <hw.chan@hotmail.com>
2015-11-07 23:35:33 -0800

    Added diagonal check

--- Assignment2a/src/assignment2a/Queen.java
+++ Assignment2a/src/assignment2a/Queen.java
@@ -30,27 +30,44 @@ public class Queen extends GamePiece {
         int toX = to.getXCoord();
         int toY = to.getYCoord();
         
-        System.out.println(fromX + ":" + fromY + " to " + toX + ":" + toY);
+        //System.out.println(fromX + ":" + fromY + " to " + toX + ":" + toY);
         
         if(to.getPiece() == null || to.getPiece().isPlayer1() != isPlayer1()) {
             // vertical movement
             if(fromX == toX) {
+                // N movement
                 if(fromY > toY)
                     return checkBlock(0,-1,fromX,fromY,toX,toY);
+                // S movement
                 else
                     return checkBlock(0,1,fromX,fromY,toX,toY);
             }
             // horizontal movement
-            if(fromY == toY) {
+            else if(fromY == toY) {
+                // W movement
                 if(fromX > toX)
                     return checkBlock(-1,0,fromX,fromY,toX,toY);
+                // E movement
                 else
                     return checkBlock(1,0,fromX,fromY,toX,toY);
             }
-            // diagonal movement
-            if(Math.abs(toX - fromX) == Math.abs(toY - fromY)) {
-                return true;
+            // NW movement
+            else if(fromX - toX == fromY - toY && fromX - toX > 0) {
+                return checkBlock(-1,-1,fromX,fromY,toX,toY);
             }
+            // SE movement
+            else if(fromX - toX == fromY - toY && fromX - toX < 0) {
+                return checkBlock(1,1,fromX,fromY,toX,toY);
+            }
+            // NE movement
+            else if(fromX - toX == (fromY - toY) * -1 && fromX - toX < 0) {
+                return checkBlock(1,-1,fromX,fromY,toX,toY);
+            }
+            // SW movement
+            else if(fromX - toX == (fromY - toY) * -1 && fromX - toX > 0) {
+                return checkBlock(-1,1,fromX,fromY,toX,toY);
+            }
+ 
         } 
         return false;
     }
